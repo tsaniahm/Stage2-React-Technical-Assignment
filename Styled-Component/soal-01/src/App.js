2f315dc7af4a00b143bca392ad79d89611922a2a
Tsaniah Munfidah <www.tsaniahmunfidah@gmail.com>
2021-11-01 12:18:57 +0700

    Soal-01 Done

---
+++ Styled-Component/soal-01/src/App.js
@@ -0,0 +1,12 @@
+import Card from "./components/Card"
+import { FlexContainer } from "./components/styles/FlexContainer"
+export default function App() {
+  return(
+    <>
+    <FlexContainer>
+          <Card></Card>
+    </FlexContainer>
+
+    </>
+  )
+}
