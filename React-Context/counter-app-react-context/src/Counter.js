b252c40ec1b0a90bb6cd27dbdff52be998330bbc
Tsaniah Munfidah <www.tsaniahmunfidah@gmail.com>
2021-11-19 09:19:01 +0700

    react context done

---
+++ React-Context/counter-app-react-context/src/Counter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { useCount } from './CountContext';
+
+const Counter = () => {
+  // set dispatch and state count from useCount()
+  const count = useCount();
+
+  return (
+    <>
+      {/* change 0 with count from context */}
+      {/* Count: 0 */}
+      Count: {count.state.count}
+      <div>
+        {/* use dispatch for onClick method */}
+        <button className="count-button" onClick={() => count.dispatch({type: 'increment'})} >+</button>
+        <button className="count-button" onClick={() => count.dispatch({type: 'decrement'})}>-</button>
+      </div>
+    </>
+  );
+}
+
+export default Counter;
